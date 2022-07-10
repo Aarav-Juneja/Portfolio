a6629a3d32cd81de0bcfe08c0e4b26ee33e5ff5a
Aarav-Juneja <aarav@aaravjuneja.com>
2022-07-09 22:35:14 -0400

    Service Workers
    Add service workers for lighthouse people

---
+++ js/sw.js
@@ -0,0 +1,36 @@
+const version = 1.0;
+const cachName = `files-${version}`
+
+// self.ski[pWaiting()
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            // cache.add("index.html")
+            cache.addAll(["/css/master.css", "js/index.js", "index.html"])
+        })
+        )
+})
+
+self.addEventListener("activate", e => {
+    // e.waitUntil(clients.claim())
+    /* 
+    e.waitUntil(
+        caches.delete("kolohe-cache")
+        )
+    */ 
+})
+
+self.addEventListener("fetch", e => {
+    e.respondWith(caches.open("files").then(cache => {
+        return cache.match(e.request.url).then(res => {
+            if (res) {
+                return fetch(e.request) || res;
+            } else {
+                // cache.add(res)
+                return fetch(e.request)
+            }
+        })
+    }))
+    // e.respondWith(fetch(e.request))
+})
