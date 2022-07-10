7da8e19e1a930149264a309774f9c0abbd6c1fc6
Aarav-Juneja <aarav@aaravjuneja.com>
2022-07-10 11:40:50 -0400

    Network first approach
    I updated my code to use mozilla stuff and a better network first approach

--- sw.js
+++ sw.js
@@ -3,6 +3,51 @@ const cacheName = `files-${version}`
 
 // self.ski[pWaiting()
 
+const putInCache = async (request, response) => {
+    const cache = await caches.open("v1");
+    await cache.put(request, response);
+}
+
+const networkFirst = async (request, preloadResponsePromise) => {
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+      console.info('using preload response', preloadResponse);
+      putInCache(request, preloadResponse.clone());
+      return preloadResponse;
+    }
+
+    // First try to get the resource from the network
+    try {
+        const responseFromNetwork = await fetch(request);
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        // Next try to get the resource from the cache
+        const responseFromCache = await caches.match(request);
+        if (responseFromCache) {
+            return responseFromCache;
+        }
+
+        // const fallbackResponse = await caches.match(fallbackUrl);
+        // if (fallbackResponse) {
+        //     return fallbackResponse;
+        // }
+
+        // when even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object
+        return new Response('Network error happened', {
+            status: 408,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    }
+
+    
+};
+
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
@@ -23,9 +68,18 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
     e.respondWith(caches.open(cacheName).then(cache => {
+        /*
         return cache.match(e.request.url).then(res => {
+            /*
             if (res) {
                 console.log('x')
+                res2 = fetch(e.request);
+                // if (res2) {
+                //     // cache.add(e.request);
+                //     return res2;
+                // } else {
+                //     return res;
+                // }
                 return fetch(e.request) || res;
             } else {
                 // cache.add(res)
@@ -33,7 +87,9 @@ self.addEventListener("fetch", e => {
                 console.log('y');
                 // return fetch(e.request)
             }
-        })
+        })*/
+
+        networkFirst(e.request, e.preloadResponse)
     }))
     // e.respondWith(fetch(e.request))
 })
