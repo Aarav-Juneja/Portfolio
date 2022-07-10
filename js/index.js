a8f7a527c7dfe6de5a61a281b60bb26c42254511
Aarav-Juneja <aarav@aaravjuneja.com>
2022-07-09 22:42:18 -0400

    Update index.js
    More bugs

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 $("#date").append(new Date().getFullYear())
 if('serviceWorker' in navigator) {
     // Register service worker
-    navigator.serviceWorker.register('sw.js').then(function(reg){
+    navigator.serviceWorker.register('js/sw.js').then(function(reg){
         console.log("SW registration succeeded. Scope is "+reg.scope);
         reg.addEventListener("updatefound", () => {
     
         })
 
         // reg.active,waiting,installing
-        
+
     }).catch(function(err){
         console.error("SW registration failed with error "+err);
     });
