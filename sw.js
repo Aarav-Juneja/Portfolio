3e3cc220f109dc273a15498167a10f0240515ea7
Aarav-Juneja <aarav@aaravjuneja.com>
2022-07-14 19:27:50 -0700

    Enable Cache first option

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const putInCache = async (request, response) => {
     await cache.put(request, response);
 }
 
-const networkFirst = async (request, preloadResponsePromise) => {
+const networkFirst = async (request, preloadResponsePromise, fallbackUrl) => {
     const preloadResponse = await preloadResponsePromise;
     if (preloadResponse) {
       console.info('using preload response', preloadResponse);
@@ -52,6 +52,45 @@ const networkFirst = async (request, preloadResponsePromise) => {
     
 };
 
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  
+    // Next try to use (and cache) the preloaded response, if it's there
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+        console.info('using preload response', preloadResponse);
+        putInCache(request, preloadResponse.clone());
+        return preloadResponse;
+    }
+
+    // Next try to get the resource from the network
+    try {
+        const responseFromNetwork = await fetch(request);
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        // const fallbackResponse = await caches.match(fallbackUrl);
+        // if (fallbackResponse) {
+        //     return fallbackResponse;
+        // }
+        
+        // when even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object
+        return new Response('Network error happened', {
+            status: 408,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    }
+  };
+
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
@@ -71,7 +110,14 @@ self.addEventListener("activate", e => {
 })
 
 self.addEventListener("fetch", e => {
-    e.respondWith(/*caches.open(cacheName).then(cache => {
+
+    if (e.request.destination = "image") {
+        e.respondWith(cacheFirst(e.request, e.preloadResponse, "../408.html"))
+    } else {
+        e.respondWith(networkFirst(e.request, e.preloadResponse, "../408.html"))
+    }
+
+    /*e.respondWith(/*caches.open(cacheName).then(cache => {
         /*
         return cache.match(e.request.url).then(res => {
             /*
@@ -94,8 +140,7 @@ self.addEventListener("fetch", e => {
         })
 
         
-    })*/
-        networkFirst(e.request, e.preloadResponse)
-        )
+    }) 
+    )*/
     // e.respondWith(fetch(e.request))
 })
