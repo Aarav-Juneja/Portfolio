9302e0592ad22ee39c3107ca5dd98e22b842f997
Aarav Juneja <aarav@aaravjuneja.com>
2025-02-18 18:17:27 -0800

    Oops. Accidentally rebased the full branch

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Portfolio
+# Astro Starter Kit: Basics
+
+## 🚀 Project Structure
+
+```text
+/
+├── public/
+│   └── favicon.svg
+├── src/
+│   ├── layouts/
+│   │   └── Layout.astro
+│   └── pages/
+│       └── index.astro
+└── package.json
+```
+
+Astro's [guide](https://docs.astro.build/en/basics/project-structure/) on project structure
+
+## 🧞 Commands
+
+All commands are run from the root of the project, from a terminal:
+
+| Command           | Action                                           |
+| :---------------- | :----------------------------------------------- |
+| `bun install`     | Installs dependencies                            |
+| `bun dev`         | Starts local dev server at `localhost:4321`      |
+| `bun build`       | Build your production site to `./dist/`          |
+| `bun run preview` | Preview your build locally, before deploying     |
+| `bunx astro ...`  | Run CLI commands like `astro add`, `astro check` |
+| `bunx --help`     | Get help using the Astro CLI                     |
+
+## 👀 Want to learn more?
+
+Feel free to check [our documentation](https://docs.astro.build) or jump into our [Discord server](https://astro.build/chat).
---
+++ src/data.ts
@@ -0,0 +1,90 @@
+import CS50x from "~/assets/images/courses/CS50X.jpg";
+import CS50AI from "~/assets/images/courses/CS50AI.webp";
+
+export default [
+    {
+        title: "CS50x",
+        description:
+            "Learned the overall basics of computer science from programs in C to websites with Flask from Harvard.",
+        image: CS50x,
+    },
+    {
+        title: "CS50AI",
+        description:
+            "Learned the basics of AI from search algorithms to neural networks from Harvard.",
+        image: CS50AI,
+    },
+    {
+        title: "CS50 Cybersecurity",
+        description:
+            "Learned the basics of cybersecurity from cryptography to web security from Harvard.",
+        image: "https://prod-discovery.edx-cdn.org/media/course/image/88391405-e329-495d-bf72-5de13bbf1846-be5135562618.small.png",
+    },
+    {
+        title: "CS50 Gaming",
+        description:
+            "Utilized tools like Love2D and Unity to create various games.",
+        image: "https://prod-discovery.edx-cdn.org/media/course/image/8d1da557-03db-4175-b881-e0480d706d96-d4fc70850897.small.jpg",
+    },
+    {
+        title: "Ultimate Web Development Bootcamp",
+        description:
+            "Created websites from raw HTML as well as complex Node.js systems",
+        image: "https://www.filepicker.io/api/file/FSIjaoI2QtKWZL65cogd",
+    },
+];
+
+export const categories = [
+    {
+        name: "AI",
+        courses: [
+            {
+                title: "CS50AI",
+                description:
+                    "Learned the basics of AI from search algorithms to neural networks from Harvard.",
+                image: CS50AI,
+            },
+        ],
+    },
+    {
+        name: "Web Development",
+        courses: [
+            {
+                title: "Ultimate Web Development Bootcamp",
+                description:
+                    "Created websites from raw HTML as well as complex Node.js systems",
+                image: "https://www.filepicker.io/api/file/FSIjaoI2QtKWZL65cogd",
+            },
+            {
+                title: "CS50x",
+                description:
+                    "Learned the overall basics of computer science from programs in C to websites with Flask from Harvard.",
+                image: CS50x,
+            },
+        ],
+    },
+    {
+        name: "Cybersecurity",
+        courses: [
+            {
+                title: "CS50 Cybersecurity",
+                description:
+                    "Learned the basics of cybersecurity from cryptography to web security from Harvard.",
+                image: "https://prod-discovery.edx-cdn.org/media/course/image/88391405-e329-495d-bf72-5de13bbf1846-be5135562618.small.png",
+            },
+        ],
+    },
+    {
+        name: "Miscellaneous",
+        courses: [
+            {
+                title: "CS50 Gaming",
+                description:
+                    "Utilized tools like Love2D and Unity to create various games.",
+                image: "https://prod-discovery.edx-cdn.org/media/course/image/8d1da557-03db-4175-b881-e0480d706d96-d4fc70850897.small.jpg",
+            },
+        ],
+    },
+];
+
+/// https://media.licdn.com/dms/image/v2/D5603AQGH8ODE349-NA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1729612122620?e=1745452800&v=beta&t=zXo9DgJ2PemhHydXxxDpkqgxhpL3Ki0lmQGyb3bWWCU
